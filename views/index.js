2aea3a5fdc2a3f842394f6e036428e2e8bfe88ee
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-05-01 17:57:09 -0400

    Refactor homepage template for better structure
    - Enhance header with proper HTML structure
    - Add h1 tag for the main title
    - Include hx-trigger for the "Show Books" button
    - Clean up form attributes for clarity and functionality

--- views/index.js
+++ views/index.js
@@ -10,22 +10,29 @@ const createHomepageTemplate = () => /*html*/ `
   </head>
 
   <body>
-    <header>My Reading List</header>
+    <header class="heading"><h1>
+      My Reading List
+    </h1></header>
 
     <main>
       <div class="book-list">
-        <button hx-get="/books" hx-target="closest div" type="button">Show Books</button>
+        <button hx-get="/books" hx-target="closest div" type="button" hx-trigger="dblclick">Show Books</button>
       </div>
 
       <div class="add-book-form">
         <h2>What do you want to read?</h2>
         
-        <form>
+        <form
+          hx-post="/books"
+          hx-target=".book-list ul"
+          hx-swap="beforeend"
+          hx-on::after-request="document.querySelector('form').reset()">
+
           <input type="text" name="title" placeholder="title" required>
 
           <input type="text" name="author" placeholder="author" required>
 
-          <button hx-post="/books" hx-target=".book-list ul" hx-swap="beforeend" hx-on::after-request="document.querySelector('form').reset()">Add Book</button>
+          <button type="submit">Add Book</button>
         </form>
       </div>
     </main>
