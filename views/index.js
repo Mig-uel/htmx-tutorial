ff4d5ed6ca364830298f7634a281b31f2dfcbcc8
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-05-01 16:11:57 -0400

    Add homepage template to render reading list structure
    - Introduces a new HTML template for the homepage
    - Sets up basic layout with sections for book list and form

---
+++ views/index.js
@@ -0,0 +1,28 @@
+const createHomepageTemplate = () => /*html*/ `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <link rel="stylesheet"  href="styles.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Reading List</title>
+  </head>
+
+  <body>
+    <header>My Reading List</header>
+
+    <main>
+      <div class="book-list">
+        <!-- book list here later -->
+      </div>
+
+      <div class="add-book-form">
+        <h2>What do you want to read?</h2>
+        <!-- form template here later -->
+      </div>
+    </main>
+  </body>
+  </html>
+`
+
+export default createHomepageTemplate
