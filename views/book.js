34d372a8555ecffed33b9af55ebb9aedee1a0355
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-05-01 18:15:20 -0400

    Add hx-get attribute to book list item for testing - Enhances the book template by adding an hx-get attribute - Prepares for future testing and interaction improvements

--- views/book.js
+++ views/book.js
@@ -1,11 +1,11 @@
 const createBookTemplate = (book) => /*html*/ `
-<li data-id="${book.id}">
+<li data-id="${book.id}" hx-get="/testing">
   <div class="details">
     <h3>${book.title}</h3>
     <p>${book.author}</p>
   </div>
 
-  <button hx-target="closest li" hx-swap="outerHTML" hx-delete="/books/${book.id}">Delete</button>
+  <button hx-target="closest li" hx-swap="outerHTML" hx-delete="/books/${book.id}" hx-on:click="event.stopPropagation()">Delete</button>
 </li>`
 
 export default createBookTemplate
