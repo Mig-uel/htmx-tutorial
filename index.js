71b54d25743e62e4a22aaa262c7c67c3f9e08291
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-05-01 21:11:35 -0400

    Update book routes to handle search and list books correctly
    - Modify the /books route to send the full list of books
    - Add a search functionality to filter books by title

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import express from 'express'
-import createHomepageTemplate from './views/index.js'
-import createBooksListTemplate from './views/books-list.js'
 import BOOKS from './data/data.js'
 import createBookTemplate from './views/book.js'
+import createBooksListTemplate from './views/books-list.js'
 import createEditFormTemplate from './views/edit.js'
+import createHomepageTemplate from './views/index.js'
 
 const PORT = 3000
 const app = express()
@@ -18,7 +18,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/books', (req, res) => {
-  return res.send(createBooksListTemplate())
+  return res.send(createBooksListTemplate(BOOKS))
 })
 
 app.post('/books', (req, res) => {
@@ -75,6 +75,14 @@ app.get('/books/edit/:id', (req, res) => {
   const book = BOOKS.find((book) => book.id === id)
 
   return res.send(createEditFormTemplate(book))
+})
+
+app.post('/books/search', (req, res) => {
+  const searchText = req.body.search.toLowerCase()
+
+  const books = BOOKS.filter((b) => b.title.toLowerCase().includes(searchText))
+
+  return res.send(createBooksListTemplate(books))
 })
 
 app.listen(PORT, () => {
