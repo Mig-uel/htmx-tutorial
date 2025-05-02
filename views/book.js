ffbfe1b744bb368bad1719b6bce358c3b94e5a3e
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-05-01 20:50:31 -0400

    Update hx-get attribute in book template for editing - Changed hx-get URL to point to the correct edit route for books - Improved clarity and functionality of the book list item

--- views/book.js
+++ views/book.js
@@ -1,11 +1,15 @@
 const createBookTemplate = (book) => /*html*/ `
-<li data-id="${book.id}" hx-get="/testing">
+<li data-id="${book.id}" hx-get="/books/edit/${book.id}" >
   <div class="details">
     <h3>${book.title}</h3>
     <p>${book.author}</p>
   </div>
 
-  <button hx-target="closest li" hx-swap="outerHTML" hx-delete="/books/${book.id}" hx-on:click="event.stopPropagation()">Delete</button>
+  <button
+  hx-target="closest li"
+  hx-swap="outerHTML"
+  hx-delete="/books/${book.id}" 
+  hx-on:click="event.stopPropagation()">Delete</button>
 </li>`
 
 export default createBookTemplate
