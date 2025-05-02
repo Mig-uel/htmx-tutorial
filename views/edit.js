b74a33fe7f4450975c54a8743faba76cf5b29cf5
Miguel <35431824+Mig-uel@users.noreply.github.com>
2025-05-01 20:57:19 -0400

    Update edit form to use hx-put for book updates
    - Changed form to use hx-put for submitting book edits
    - Updated button text to "Edit Book" for clarity

--- views/edit.js
+++ views/edit.js
@@ -1,10 +1,10 @@
 const createEditFormTemplate = (book) => /*html*/ `
-<form>
+<form hx-put="/book/${book.id}">
   <input type="text" name="title" placeholder="title" value="${book.title}" required>
 
   <input type="text" name="author" placeholder="author" value="${book.author}" required>
 
-  <button type="submit">Add Book</button>
+  <button type="submit">Edit Book</button>
 </form>
 `
 
